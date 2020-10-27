e3cede2cc47f1ae557107f3f4887b265b1ed50df
sinisterkyle <70821920+sinisterkyle@users.noreply.github.com>
2020-10-27 15:09:11 -0700

    Update clickevents.js

--- clickevents.js
+++ clickevents.js
@@ -1,14 +1,18 @@
 const buttons = document.querySelectorAll("button");
 const body = document.querySelector("body");
+
 document.getElementById('1');
     addEventListener("click", function() {
-document.getElementById("demo1").innerHTML = "Foo";
+    document.getElementById("demo1").innerHTML = "Foo";
     });
+
 document.getElementById('2');
     addEventListener("click", function() {
-document.getElementById("demo2").innerHTML = "Bar";
+    document.getElementById("demo2").innerHTML = "Bar";
     });
+
 document.getElementById('3');
     addEventListener("click", function() {
-document.getElementById("demo3").innerHTML = "FooBar";
+    document.getElementById("demo3").innerHTML = "FooBar";
+
 });
