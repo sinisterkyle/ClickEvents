b41d56017f64d067d438f574057c4f36dc8708d4
sinisterkyle <70821920+sinisterkyle@users.noreply.github.com>
2020-11-19 17:39:46 -0800

    Update clickevents.js

--- clickevents.js
+++ clickevents.js
@@ -1,16 +1,20 @@
-const button = document.querySelector("button");
+const fooButton = document.getElementById("fooButton");
+const barButton = document.getElementById("barButton");
+const fooBarButton = document.getElementById("fooBarButton");
 const body = document.querySelector("body");
-button.addEventListener("click", event => {
 
-if (event.button == 0) {
-    document.getElementById("demo1").innerHTML = "Foo";
-    };
-
-if (event.button == 1) {
-    document.getElementById("demo2").innerHTML = "Bar";
-    };
-
-if (event.button == 2) {
-    document.getElementById("demo3").innerHTML = "FooBar";
-    };
-});
+    fooButton.addEventListener("click", function(event) {
+        const h3 = document.createElement('h3');
+        h3.textContent = "Foo";
+        body.appendChild(h3);
+    })
+    barButton.addEventListener("click", function(event) {
+        const h31 = document.createElement('h3');
+        h31.textContent = "Bar";
+        body.appendChild(h31);
+    })    
+    fooBarButton.addEventListener("click", function(event) {
+        const h2 = document.createElement('h2');
+        h2.textContent = "FooBar";
+        body.appendChild(h2);
+    })
